2adbabb5601438f70909f8fa9d37ab1b7b78082e
pch-emeric <emeric.boyadjian@pch.click>
2022-11-30 11:24:47 +0100

    create pokemon list page and add some style

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -17,12 +17,12 @@ class PokemonController extends AbstractController
         $this->client = $client;
     }
 
-    #[Route('/pokemon', name: 'app_pokemon')]
-    public function getOnePokemonFromApi(): Response
+    #[Route('/pokemon', name: 'pokemons')]
+    public function getPokemons(): Response
     {
         $response = $this->client->request(
             'GET',
-            'https://pokeapi.co/api/v2/pokemon/charizard'
+            'https://pokeapi.co/api/v2/pokemon/',
         );
 
         // $statusCode = $response->getStatusCode();
@@ -30,18 +30,50 @@ class PokemonController extends AbstractController
         $content = $response->getContent();
         $content = $response->toArray();
 
-        $pokemon = new Pokemon();
+        $pokemonList = [];
 
-        $pokemon->setPokeId($content["id"]);
-        $pokemon->setName($content["name"]);
-        $pokemon->setWeight($content["weight"]);
-        $pokemon->setPokeOrder($content["order"]);
-        $pokemon->setBaseExperience($content["base_experience"]);
-        $pokemon->setTypes($content["types"]);
-        $pokemon->setStats($content["stats"]);
-        $pokemon->setSpecies($content["species"]);
+        foreach ($content["results"] as $key => $value) {
+            preg_match("/([^\/]+)(?=[^\/]*\/?$)/", $value["url"], $matches);
+            $pokeId = $matches[0];
+            $img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" . $pokeId . ".png";
 
-        $img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" . $pokemon->getPokeId() . ".png";
+            array_push($pokemonList, ["name" => $value["name"], "img" => $img]);
+        }
+
+        return $this->render('pokemon/search.html.twig', [
+            'controller_name' => 'PokemonController',
+            'list' => $pokemonList,
+        ]);
+    }
+
+    #[Route('/pokemon/{name}', name: 'one_pokemon')]
+    public function getOnePokemonFromApi(string $name): Response
+    {
+        $response = $this->client->request(
+            'GET',
+            'https://pokeapi.co/api/v2/pokemon/' . $name,
+        );
+
+        // $statusCode = $response->getStatusCode();
+        // $contentType = $response->getHeaders()['content-type'][0];
+        $content = $response->getContent();
+        $content = $response->toArray();
+
+        if ($name) {
+            $pokemon = new Pokemon();
+
+            $pokemon->setPokeId($content["id"]);
+            $pokemon->setName($content["name"]);
+            $pokemon->setWeight($content["weight"]);
+            $pokemon->setPokeOrder($content["order"]);
+            $pokemon->setBaseExperience($content["base_experience"]);
+            $pokemon->setTypes($content["types"]);
+            $pokemon->setStats($content["stats"]);
+            $pokemon->setSpecies($content["species"]);
+
+            $img = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" . $pokemon->getPokeId() . ".png";
+        }
+
 
         return $this->render('pokemon/index.html.twig', [
             'controller_name' => 'PokemonController',
